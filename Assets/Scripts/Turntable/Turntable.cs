34f004091f47ea7d328a09b83bfd69b458690537
Magnus <magnuseilersen@yahoo.dk>
2020-10-21 16:29:15 +0200

    Remade the turntable. It now has an upper and lower part, it is smaller for easier rotation and it is a torus instead of a cylinder.

--- Assets/Scripts/Turntable/Turntable.cs
+++ Assets/Scripts/Turntable/Turntable.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Turntable : MonoBehaviour
 {
     public GameObject handlePrefab;
+    public GameObject turntableRingPrefab;
     public Transform childObject;
 
     public int elementCount = 12;
     public float radius = .5f;
     public float height = 0.5f;
+    public float rotationSensitivity = 5;
 
     private void Awake()
     {
@@ -29,15 +31,26 @@ public class Turntable : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        //create lower handles
+        CreateTurntableHandles(0.0f);
+        CreateTurntableHandles(0.8f);
+        
+        
+    }
+
+    void CreateTurntableHandles(float spawnHeight)
+    {
         for (int i = 0; i < elementCount; i++)
         {
             float delta = i * 2 * Mathf.PI / elementCount;
             var go = Instantiate(handlePrefab);
-            go.transform.localPosition = new Vector3(Mathf.Sin(delta) * radius, 0, Mathf.Cos(delta) * radius);
+            go.transform.localPosition = new Vector3(Mathf.Sin(delta) * radius, spawnHeight, Mathf.Cos(delta) * radius);
             go.transform.localRotation = Quaternion.Euler(0, 180 + i * 360.0f / elementCount, 0);
             go.GetComponent<TurntableController>()._turntable = this;
             go.transform.parent = this.transform;
         }
+        //create ring model
+        var ring = Instantiate(turntableRingPrefab);
+        ring.transform.localPosition = new Vector3(0, spawnHeight, 0);
     }
-
 }
